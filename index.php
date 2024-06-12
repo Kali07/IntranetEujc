a0f742ac8d6bee642095a99e3de021628bb789b6
WPJ008 <wawitephilemon@gmail.com>
2024-06-12 22:47:03 +0200

    modification

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link href="login.css"   rel="stylesheet" >
+    <link href="index.css"   rel="stylesheet" >
     
 </head>
 <body>

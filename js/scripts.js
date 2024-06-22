a85ceea894d497479a66ef6ce73a2ca837891c03
Kali07 <richardmulamba16@gmail.com>
2024-06-22 12:39:50 +0200

    version centralisé du js

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,54 @@
-// js/scripts.js
-
 function toggleProfile() {
     const profileInfo = document.getElementById('profile-info');
     profileInfo.style.display = profileInfo.style.display === 'block' ? 'none' : 'block';
 }
 
+function loadContent(url, targetElementId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(targetElementId).innerHTML = data;
+        })
+        .catch(error => console.error('Error loading content:', error));
+}
+
 document.getElementById('notes').addEventListener('click', () => {
-    document.getElementById('content').innerHTML = `
-        <h1>Notes</h1>
-        <textarea placeholder="Prenez vos notes ici..." style="width: 100%; height: 200px;"></textarea>
-    `;
+    loadContent('includes/load_notes.php', 'content');
 });
 
 document.getElementById('qcm').addEventListener('click', () => {
-    document.getElementById('content').innerHTML = `
-        <h1>QCM</h1>
-        <p>Cette section sera dédiée aux QCM.</p>
-    `;
+    loadContent('includes/load_qcm.php', 'content');
 });
 
 document.getElementById('stations').addEventListener('click', () => {
-    document.getElementById('content').innerHTML = `
-        <h1>Stations</h1>
-        <p>Cette section sera dédiée aux stations.</p>
-    `;
+    loadContent('includes/load_stations.php', 'content');
+}); 
+
+document.getElementById('evenement').addEventListener('click', () => {
+    loadContent('includes/load_evenements.php', 'content');
+}); 
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Fonction pour ouvrir le modal
+    function openModal() {
+        document.getElementById("myModal").style.display = "block";
+    }
+
+    // Fonction pour fermer le modal
+    function closeModal() {
+        document.getElementById("myModal").style.display = "none";
+    }
+
+    // Ajouter les événements pour ouvrir et fermer le modal
+    document.getElementById("openModalBtn").addEventListener("click", openModal);
+    document.querySelector(".close").addEventListener("click", closeModal);
+
+    // Fermer le modal si l'utilisateur clique en dehors de celui-ci
+    window.addEventListener("click", function(event) {
+        if (event.target == document.getElementById("myModal")) {
+            closeModal();
+        }
+    });
 });
